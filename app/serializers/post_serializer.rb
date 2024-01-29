f239b0cf6b4fd1778784b03e93c8239e913a07f8
Wladimir Oliveira Souza <wladimir.souza@rebase.com.br>
2024-01-29 18:02:10 -0300

    Updating serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,9 +1,13 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :description, :user_id, :image_url
+  attributes :id, :description, :user_id, :image_url, :user
 
   def image_url
     if object.image.attached?
       Rails.application.routes.url_helpers.rails_blob_path(object.image, only_path: true)
     end
+  end
+
+  def user
+    UserSerializer.new(object.user).attributes
   end
 end
